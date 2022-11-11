946da8f07af141894f142d1aa1c921f573598296
Marco Garcia <marcogarcia.gon@gmail.com>
2022-11-10 21:33:32 -0600

    Alta y busquedas funcionando

--- js/classes.js
+++ js/classes.js
@@ -4,18 +4,22 @@ let addBtn = document.querySelector('#submit')
 let retrieveStudents = document.querySelector('#getStudents')
 
 class Student {
-    constructor(firstName, lastName, age, assignment, score) {
+    constructor(firstName, lastName, age, assignment, score, edit) {
         this.firstName = firstName
         this.lastName = lastName
         this.age = age
         this.assignment = assignment
         this.score = score
+        this.edit = edit
     }
+
+
 
 
 }
 
-const printTable = (firstName, lastName, age, assignment, score) => {
+// Create new student in table
+const printTable = (firstName, lastName, age, assignment, score, edit) => {
     let getTable = document.getElementById('studentsList')
     let newRow = document.createElement('tr')
     newRow.innerHTML = `
@@ -24,15 +28,137 @@ const printTable = (firstName, lastName, age, assignment, score) => {
         <td>${age}</td>
         <td>${assignment}</td>
         <td>${score}</td>
+        <td>${edit}</td>
         `
     getTable.appendChild(newRow)
 }
 
+// Erase message searchByName
+const eraseMessageSearchName = () => {
+    setTimeout(() => {
+        document.querySelector('#messageSearchByName').innerHTML = ''
+    }, 2000)
+}
+
+// Erase message success
 const eraseMessage = () => {
     setTimeout(() => {
         document.querySelector('#message').innerHTML = ''
     }, 2000)
 }
+
+// Clear inputs
+function clearInput() {
+    name = ''
+    lastName = ''
+    age = ''
+    score = ''
+}
+
+// Local storage of data
+const localSave = (fnAdd) => {
+    for (student in fnAdd) {
+        localStorage.setItem("student", JSON.stringify(students))
+    }
+
+}
+
+// Students average
+const average = (arr) => {
+    for (let num in arr.score) {
+        let sum = 0
+        let total = sum += arr[num].score
+        let getRes = document.querySelector('#average')
+        return getRes.innerHTML = total / arr.score.length
+    }
+
+
+    // for (let i = 0; i < students.length; i++) {
+    //     if (students[i].score != undefined) {
+    //         let scores = []
+    //         scores.push(students[i].scores)
+    //         for (num in scores) {
+    //             let getAvgField = document.querySelector('#avgClass')
+    //             let add = 0
+    //             add += num
+    //             let avg = add / scores.length
+    //             return getAvgField.innerHTML = avg
+
+    //         }
+    //     }
+    // }
+}
+
+// Search student by name
+const searchByName = () => {
+    let searchInput = document.querySelector('#searchInput').value
+    for (let i = 0; i < students.length; i++) {
+        if (searchInput == students[i].firstName) {
+            // console.log(students[i])
+            let modalContainer = document.querySelector('.modal-container')
+            modalContainer.style.display = 'flex'
+            let getTable = document.querySelector('#results')
+            let result = document.createElement('tr')
+            result.innerHTML = `
+            <td>${students[i].firstName}</td>
+            <td>${students[i].lastName}</td>
+            <td>${students[i].age}</td>
+            <td>${students[i].assignment}</td>
+            <td>${students[i].score}</td>
+            `
+            getTable.appendChild(result)
+
+            // Removing modal on click elsewhere
+            modalContainer.addEventListener('click', function() {
+                modalContainer.style.display = 'none'
+            })
+        } else {
+            let getMessage = document.querySelector('#messageSearchByName')
+            getMessage.innerHTML = 'Alumno no encontrado'
+
+            // Calling function to erase fail message
+            eraseMessageSearchName()
+        }
+    }
+}
+
+// Search student by name
+const searchByLast = () => {
+    let searchInput = document.querySelector('#searchInputLast').value
+    for (let i = 0; i < students.length; i++) {
+        if (searchInput == students[i].lastName) {
+            // console.log(students[i])
+            let modalContainer = document.querySelector('.modal-container')
+            modalContainer.style.display = 'flex'
+            let getTable = document.querySelector('#results')
+            let result = document.createElement('tr')
+            result.innerHTML = `
+            <td>${students[i].firstName}</td>
+            <td>${students[i].lastName}</td>
+            <td>${students[i].age}</td>
+            <td>${students[i].assignment}</td>
+            <td>${students[i].score}</td>
+            `
+            getTable.appendChild(result)
+
+            // Removing modal on click elsewhere
+            modalContainer.addEventListener('click', function() {
+                modalContainer.style.display = 'none'
+            })
+        } else {
+            let getMessage = document.querySelector('#messageSearchByName')
+            getMessage.innerHTML = 'Alumno no encontrado'
+
+            // Calling function to erase fail message
+            eraseMessageSearchName()
+        }
+    }
+}
+
+
+
+
+// Add Students and unleashed functions
 
 const addStudent = () => {
     let name = document.querySelector('#firstName').value
@@ -40,24 +166,29 @@ const addStudent = () => {
     let age = document.querySelector('#age').value
     let assignment = document.querySelector('#assignment').value
     let score = document.querySelector('#score').value
+    let edit = `<button onclick ="editSudent()">Editar</button>`
     document.querySelector('#message').innerHTML = 'Alumno añadido correctamente'
 
     // Calling function to erase message
     eraseMessage()
 
+    // Erase inputs
+    clearInput()
+
     // Calling printTable with same data in fields
-    printTable(name, lastName, age, assignment, score)
+    printTable(name, lastName, age, assignment, score, edit)
 
     // Saving into new prototype array
-    return students.push(new Student(name, lastName, age, assignment, score))
+    students.push(new Student(name, lastName, age, assignment, score))
 
-
-
+    //Saving to local storage
+    localSave(students)
 
 }
 
+// const loadStored = () => {
+//     for ()
+//     localStorage.getItem()
+// }
 
-
-
-
-// addBtn.addEventListener('click', printTable(students))
+// document.addEventListener('DOMContentLoaded', loadStored())
